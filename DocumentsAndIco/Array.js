d9154c48587c6a145a58c4489bf27bfb92c8f4c8
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-03-28 09:41:26 +1000

    Stats Completed

--- DocumentsAndIco/Array.js
+++ DocumentsAndIco/Array.js
@@ -1,19 +1,40 @@
 mainArray = {
-    "Name":["Angus Walker","Samuel"],
-    "Team":["Loose Gooses","5 Musketeers"],
-    "PPG":[2.5,3],
-    "Finishes":[1,5],
-    "Midranges":[2,8],
-    "threepointers":[3,21],
-    "AccoladesOne":["MVP","dwdw"],
-    "AccoladesTwo":["Teammate of the Term","fet"],
-    "AccoladesThree":["QVP","iuh"],
-    "AccoladesFour":["DWd","iujj"],
-    "AccoladesFive":["wdds","mmm"],
-    "AccoladesSix":["abv","h2ww"],
-    "AccoladesSeven":["fwdws","poiu"],
-    "AccoladesEight":["nhtg","tyhn"],
-    "History":["A cool dude","A not so cool dude"],
-    "TeamImage":["../Images/LG_Final.png","../Images/5M_Final.png"],
-    "PlayerImage":["../Images/Players/Angus.png","../Images/Players/Sam.png"]
-  }
+  "Name":["Angus Walker","Samuel"],
+  "Team":["Loose Gooses","5 Musketeers"],
+  "PPG":[2.5,3.5],
+  "TP":[5,4],
+  "FPG":[1.5,4.5],
+  "TF":[3,5],
+  "MPG":[4.5,5.5],
+  "TM":[9,6],
+  "TPG":[3.5,6.5],
+  "TT":[7,7],
+  "AccoladesOne":["MVP","dwdw"],
+  "AccoladesTwo":["LVP","fet"],
+  "AccoladesThree":["QVP","iuh"],
+  "AccoladesFour":["DWd","iujj"],
+  "AccoladesFive":["wdds","mmm"],
+  "AccoladesSix":["abv","h2ww"],
+  "AccoladesSeven":["fwdws","poiu"],
+  "AccoladesEight":["nhtg","tyhn"],
+  "History":["A cool dude","A not so cool dude"],
+  "TeamImage":["../Images/LG_Final.png","../Images/5M_Final.png"],
+  "PlayerImage":["../Images/Players/Angus.png","../Images/Players/Sam.png"]
+}
+
+
+dayOneArray = {
+  "Date":["28-Mar","28-Mar"],
+  "Points":[1,5],
+  "Finishes":[2,6],
+  "Midranges":[3,7],
+  "ThreePointers":[4,8]
+}
+
+dayTwoArray = {
+  "Date":["29-Mar","29-Mar"],
+  "Points":[12,52],
+  "Finishes":[12,36],
+  "Midranges":[37,57],
+  "ThreePointers":[46,18]
+}
--- js/Stats.js
+++ js/Stats.js
@@ -1,33 +1,83 @@
-var sportsData = [
-    { Id: 'game1', Game: 'Badminton' },
-    { Id: 'game2', Game: 'Football' },
-    { Id: 'game3', Game: 'Tennis' }
-];
-//initiate the DropDownList
-var dropDownListObject = new ej.dropdowns.DropDownList({
-    // bind the sports Data to datasource property
-    dataSource: sportsData,
-    // maps the appropriate column to fields property
-    fields: { text: 'Game', value: 'Id' },
-    //set the placeholder to DropDownList input
-    placeholder: "Select a game",
-    // bind change event handler
-    change: onChange
-});
-//render the component
-dropDownListObject.appendTo('#ddlelement');
- 
-// Set value dynamically
-document.getElementById('btn').onclick = () => {
-    dropDownListObject.value = 'game3';
-};
-function onChange(args) {
-    var element = document.createElement('p');
-    if (args.isInteracted) {
-        element.innerText = 'Changes happened by Interaction';
-    }
-    else {
-        element.innerText = 'Changes happened by programmatic';
-    }
-    document.getElementById('event').append(element);
+let images = ['./images/one.jpg','./images/two.jpg','./images/three.jpg'];
+let main = document.getElementById("main")
+let points = document.getElementById("points")
+let finishes = document.getElementById("finishes")
+let midranges = document.getElementById("midranges")
+let threepointers = document.getElementById("threepointers")
+let accolade1 = document.getElementById("accolade1")
+let accolade2 = document.getElementById("accolade2")
+let accolade3 = document.getElementById("accolade3")
+let accolade4 = document.getElementById("accolade4")
+let accolade5 = document.getElementById("accolade5")
+let accolade6 = document.getElementById("accolade6")
+let accolade7 = document.getElementById("accolade7")
+let accolade8 = document.getElementById("accolade8")
+let history = document.getElementById("history")
+let TeamImage = document.getElementById("TeamImage")
+let PlayerImage = document.getElementById("PlayerImage")
+let number = -1
+let mainArea = document.getElementById("mainArea")
+mainArea.style.display = "none"
+
+
+  let selectDiv = document.getElementById('changer');
+  selectDiv.addEventListener('change',function(e){
+
+switch (e.target.value) {
+  case 'zero' :
+    number = 0
+    break;
+  case 'one' :
+    number = 1
+    break;
+  case 'two' :
+    number = 2
+    break;
 }
+RunMain()
+  });
+
+function RunMain() {
+    points.innerHTML = String(mainArray.PPG[number]) + "/" + String(mainArray.TP[number])
+    finishes.innerHTML = String(mainArray.FPG[number]) + "/" + String(mainArray.TF[number])
+    midranges.innerHTML = String(mainArray.MPG[number]) + "/" + String(mainArray.TM[number])
+    threepointers.innerHTML = String(mainArray.TPG[number]) + "/" + String(mainArray.TT[number])
+    accolade1.innerHTML = mainArray.AccoladesOne[number]
+    accolade2.innerHTML = mainArray.AccoladesTwo[number]
+    accolade3.innerHTML = mainArray.AccoladesThree[number]
+    accolade4.innerHTML = mainArray.AccoladesFour[number]
+    accolade5.innerHTML = mainArray.AccoladesFive[number]
+    accolade6.innerHTML = mainArray.AccoladesSix[number]
+    accolade7.innerHTML = mainArray.AccoladesSeven[number]
+    accolade8.innerHTML = mainArray.AccoladesEight[number]
+    history.innerHTML = mainArray.History[number]
+    TeamImage.src = mainArray.TeamImage[number]
+    PlayerImage.src = mainArray.PlayerImage[number]
+    mainArea.style.display = ''
+        
+
+
+
+
+
+
+    /* Past Results Section  */
+
+    
+    dayOne = document.getElementById("dOne")
+
+    dayOne.innerHTML = "<td>" + String(dayOneArray.Date[number]) + "</td>" + 
+    "<td>" + String(dayOneArray.Points[number]) + "</td>" + 
+    "<td>" + String(dayOneArray.Finishes[number]) + "</td>" + 
+    "<td>" + String(dayOneArray.Midranges[number]) + "</td>" + 
+    "<td>" + String(dayOneArray.ThreePointers[number]) + "</td>"
+
+    dayTwo = document.getElementById("dTwo")
+
+    dayTwo.innerHTML = "<td>" + String(dayTwoArray.Date[number]) + "</td>" + 
+    "<td>" + String(dayTwoArray.Points[number]) + "</td>" + 
+    "<td>" + String(dayTwoArray.Finishes[number]) + "</td>" + 
+    "<td>" + String(dayTwoArray.Midranges[number]) + "</td>" + 
+    "<td>" + String(dayTwoArray.ThreePointers[number]) + "</td>"
+
+}   
