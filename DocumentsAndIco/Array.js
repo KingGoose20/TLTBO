3a04004d2420d43f07c8c87a86a635ec2b74b6dc
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-03-27 17:04:10 +1000

    Stats Complete

---
+++ DocumentsAndIco/Array.js
@@ -0,0 +1,19 @@
+mainArray = {
+    "Name":["Angus Walker","Samuel"],
+    "Team":["Loose Gooses","5 Musketeers"],
+    "PPG":[2.5,3],
+    "Finishes":[1,5],
+    "Midranges":[2,8],
+    "threepointers":[3,21],
+    "AccoladesOne":["MVP","dwdw"],
+    "AccoladesTwo":["LVP","fet"],
+    "AccoladesThree":["QVP","iuh"],
+    "AccoladesFour":["DWd","iujj"],
+    "AccoladesFive":["wdds","mmm"],
+    "AccoladesSix":["abv","h2ww"],
+    "AccoladesSeven":["fwdws","poiu"],
+    "AccoladesEight":["nhtg","tyhn"],
+    "History":["A cool dude","A not so cool dude"],
+    "TeamImage":["../Images/LG_Final.png","../Images/5M_Final.png"],
+    "PlayerImage":["../Images/Players/Angus.png","../Images/Players/Sam.png"]
+  }
