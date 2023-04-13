09d646a2cb01f2acc1e033ac88a9ee7449fb4734
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-04-13 17:53:11 +1000

    Update Array.js

--- DocumentsAndIco/Array.js
+++ DocumentsAndIco/Array.js
@@ -1,34 +1,25 @@
 mainArray = {
-  "Name":["Angus Walker","Samuel"],
-  "Team":["Loose Gooses","5 Musketeers"],
-  "PPG":[2.5,3.5],
-  "TP":[5,4],
-  "FPG":[1.5,4.5],
-  "TF":[3,5],
-  "MPG":[4.5,5.5],
-  "TM":[9,6],
-  "TPG":[3.5,6.5],
-  "TT":[7,7],
-  "AccoladesOne":["MVP","dwdw"],
-  "AccoladesTwo":["LVP","fet"],
-  "AccoladesThree":["QVP","iuh"],
-  "AccoladesFour":["DWd","iujj"],
-  "AccoladesFive":["wdds","mmm"],
-  "AccoladesSix":["abv","h2ww"],
-  "AccoladesSeven":["fwdws","poiu"],
-  "AccoladesEight":["nhtg","tyhn"],
-  "History":["A cool dude","A not so cool dude"],
-  "TeamImage":["../Images/LG_Final.png","../Images/5M_Final.png"],
-  "PlayerImage":["../Images/Players/Angus.png","../Images/Players/Sam.png"]
-}
-
-
-dayOneArray = {
-  "Date":["28-Mar","28-Mar"],
-  "Points":[4122,5],
-  "Finishes":[2,6],
-  "Midranges":[3,7],
-  "ThreePointers":[4,8]
+  "Name":["Jasper Collier","Conor Farrington","Alexander Galt","Rudy Hoschke","Michael Iffland","Lukas Johnston","Clarrie Jones","William Kim","Samuel McConaghy","Ryan Pattemore","Nicholas Szogi","Christopher Tomkinson","Angus Walker","Willie Weekes","Mitch Yue"],
+  "Team":["5 Musketeers","5 Musketeers","Wet Willies","Wet Willies","5 Musketeers","5 Musketeers","Wet Willies","Loose Gooses","5 Musketeers ","Wet Willies","Wet Willies","Loose Gooses","Loose Gooses","Wet Willies","Loose Gooses"],
+  "PPG":[2.5,3.5,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  "TP":[5,4,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  "FPG":[1.5,4.5,7.5,10.5,13.5,16.5,19.5,22.5,25.5,28.5,31.5,34.5,37.5,40.5,43.5],
+  "TF":[3,5,7,9,11,13,15,17,19,21,23,25,27,29,31],
+  "MPG":[4.5,5.5,6.5,7.5,8.5,9.5,10.5,11.5,12.5,13.5,14.5,15.5,16.5,17.5,18.5],
+  "TM":[9,6,3,0,-3,-6,-9,-12,-15,-18,-21,-24,-27,-30,-33],
+  "TPG":[3.5,6.5,9.5,12.5,15.5,18.5,21.5,24.5,27.5,30.5,33.5,36.5,39.5,42.5,45.5],
+  "TT":[7,7,7,7,7,7,7,7,7,7,7,7,7,7,7],
+  "AccoladesOne":["MVP","dwdw","MVP","dwdw","MVP","dwdw","MVP","dwdw","MVP","dwdw","MVP","dwdw","MVP","dwdw","MVP"],
+  "AccoladesTwo":["LVP","fet","LVP","fet","LVP","fet","LVP","fet","LVP","fet","LVP","fet","LVP","fet","LVP"],
+  "AccoladesThree":["QVP","iuh","QVP","iuh","QVP","iuh","QVP","iuh","QVP","iuh","QVP","iuh","QVP","iuh","QVP"],
+  "AccoladesFour":["DWd","iujj","DWd","iujj","DWd","iujj","DWd","iujj","DWd","iujj","DWd","iujj","DWd","iujj","DWd"],
+  "AccoladesFive":["wdds","mmm","wdds","mmm","wdds","mmm","wdds","mmm","wdds","mmm","wdds","mmm","wdds","mmm","wdds"],
+  "AccoladesSix":["abv","h2ww","abv","h2ww","abv","h2ww","abv","h2ww","abv","h2ww","abv","h2ww","abv","h2ww","abv"],
+  "AccoladesSeven":["fwdws","poiu","fwdws","poiu","fwdws","poiu","fwdws","poiu","fwdws","poiu","fwdws","poiu","fwdws","poiu","fwdws"],
+  "AccoladesEight":["nhtg","tyhn","nhtg","tyhn","nhtg","tyhn","nhtg","tyhn","nhtg","tyhn","nhtg","tyhn","nhtg","tyhn","nhtg"],
+  "History":["A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude","A not so cool dude","A cool dude"],
+  "TeamImage":["../Images/5M_Final.png","../Images/5M_Final.png","../Images/WW_Final.png","../Images/WW_Final.png","../Images/5M_Final.png","../Images/5M_Final.png","../Images/WW_Final.png","../Images/LG_Final.png","../Images/WW_Final.png","../Images/WW_Final.png","../Images/WW_Final.png","../Images/LG_Final.png","../Images/LG_Final.png","../Images/WW_Final.png","../Images/LG_Final.png"],
+  "PlayerImage":["../Images/Players/Jasper.png","../Images/Players/Conor.png","../Images/Players/Alex.png","../Images/Players/Rudy.png","../Images/Players/Michael.png","../Images/Players/Lukas.png","../Images/Players/Clarrie.png","../Images/Players/Kimmy.png","../Images/Players/SamM.png","../Images/Players/Ryan.png","../Images/Players/Nick.png","../Images/Players/Chris.png","../Images/Players/Angus.png","../Images/Players/Willie.png","../Images/Players/Mitch.png"]
 }
 
 dayTwoArray = {
