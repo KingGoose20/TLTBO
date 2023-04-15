b3ec818f6703d15c90ed17328c77e2d3c4359b41
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-04-15 11:39:41 +1000

    Stat Tracker

---
+++ Stat Tracker/StatTracker.js
@@ -0,0 +1,189 @@
+LooseGooses = ["Angus Walker", "William Kim", "Chris Tomkinson", "Mitch Yue"]
+WetWillies = ["Rudy Hoschke", "Alexander Galt", "Nick Szogi", "Clarrie Jones", "Ryan Pattemore", "Willie Weekes"]
+Musketeers = ["Samuel McConaghy", "Lukas Johnston", "Michael Iffland", "Conor Farrington", "Jasper Collier"]
+
+aTeam = ""
+bTeam = ""
+cTeam = ""
+gameNumber = 1
+LGstreak = 0
+WWstreak = 0
+Mstreak = 0
+
+
+function start() {
+    document.getElementById("aTeam").disabled = true;
+    document.getElementById("bTeam").disabled = true;
+    document.getElementById("startButton").disabled = true;
+
+    switch (document.getElementById("aTeam").value) {
+        case "zero" :
+            aTeam = "Loose Gooses"
+            break;
+        case "one" :
+            aTeam = "5 Musketeers"
+            break;
+        case "two" :
+            aTeam = "Wet Willies"
+            break;
+    }
+
+    switch (document.getElementById("bTeam").value) {
+        case "three" :
+            bTeam = "Loose Gooses"
+            break;
+        case "four" :
+            bTeam = "5 Musketeers"
+            break;
+        case "five" :
+            bTeam = "Wet Willies"
+            break;
+    }
+
+    if (aTeam == "Loose Gooses" && bTeam == "Wet Willies") {
+        cTeam = "5 Musketeers"
+    } else if (aTeam == "Loose Gooses" && bTeam == "5 Musketeers") {
+        cTeam = "Wet Willies"
+    } else if (aTeam == "5 Musketeers" && bTeam == "Loose Gooses") {
+        cTeam = "Wet Willies"
+    } else if (aTeam == "5 Musketeers" && bTeam == "Wet Willies") {
+        cTeam = "Loose Gooses"
+    } else if (aTeam == "Wet Willies" && bTeam == "5 Musketeers") {
+        cTeam = "Loose Gooses"
+    } else {
+        cTeam = "5 Musketeers"
+    }
+
+    assignPlayers(aTeam)
+
+    assignPlayers(bTeam)
+}
+
+
+
+function assignPlayers(team) {
+    select = document.getElementById("Scorer")
+    if (team == "Loose Gooses") {
+        for (i = 0; i<LooseGooses.length; i++){
+            var opt = document.createElement('option');
+            opt.value = i;
+            opt.innerHTML = LooseGooses[i];
+            select.appendChild(opt);
+        }
+    } else if (team == "5 Musketeers") {
+        for (i = 0; i<Musketeers.length; i++){
+            var opt = document.createElement('option');
+            opt.value = i + 4;
+            opt.innerHTML = Musketeers[i];
+            select.appendChild(opt);
+        }
+    } else {
+        for (i = 0; i<WetWillies.length; i++){
+            var opt = document.createElement('option');
+            opt.value = i + 9 ;
+            opt.innerHTML = WetWillies[i];
+            select.appendChild(opt);
+        }
+    }
+}
+
+
+function logDetails() {
+    Scorer = document.getElementById("Scorer")
+    Type = document.getElementById("Type")
+    table = document.getElementById("loggedDetails");
+    row = table.insertRow(-1);
+    winner = ""
+    loser = ""
+    if (Scorer.options[Scorer.selectedIndex].value < 4) {
+        winner = "Loose Gooses"
+        LGstreak = range(LGstreak, 1, 1, 100)
+        wStreak = LGstreak
+    } else if (Scorer.options[Scorer.selectedIndex].value < 9) {
+        winner = "5 Musketeers"
+        Mstreak = range(Mstreak, 1, 1, 100)
+        wStreak = Mstreak
+    } else {
+        winner = "Wet Willies"
+        WWstreak = range(WWstreak, 1, 1, 100)
+        wStreak = WWstreak
+    }
+
+    if (winner == aTeam) {
+        loser = bTeam
+    } else {
+        loser = aTeam
+    }
+
+    if (loser == "Loose Gooses") {
+        LGstreak = range(LGstreak, -1, -100, -1)
+        lStreak = LGstreak * -1
+    } else if (loser == "Wet Willies") {
+        WWstreak = range(WWstreak, -1, -100, -1)
+        lStreak = WWstreak * -1
+    } else {
+        Mstreak = range(Mstreak, -1, -100, -1)
+        lStreak = Mstreak * -1
+    }
+
+
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+
+    cell1.innerHTML = gameNumber;
+    cell2.innerHTML = winner;
+    cell3.innerHTML = loser;
+    cell4.innerHTML = Scorer.options[Scorer.selectedIndex].text;
+    cell5.innerHTML = Type.options[Type.selectedIndex].text;
+    cell6.innerHTML = wStreak;
+    cell7.innerHTML = lStreak;
+    gameNumber += 1;
+    newTeams(winner)
+    alert("Logged!")
+}
+
+function range(number, change, min, max) {
+    number += change
+    if (change < 0) {
+        if (number > max) {
+            number = max
+        }
+    } else {
+        if (number < min) {
+            number = min
+        }
+    }
+
+    return number;
+}
+
+
+
+
+function newTeams(winner) {
+    localA = aTeam
+    localB = bTeam
+    localC = cTeam
+
+    if (winner == aTeam) {
+        cTeam = bTeam
+        bTeam = localC
+    } else {
+        cTeam = aTeam
+        aTeam = bTeam
+        bTeam = localC
+    }
+
+    for (i = document.getElementById("Scorer").length; i > 0; i--) {
+        document.getElementById("Scorer").remove(i)
+    }
+
+    assignPlayers(aTeam)
+    assignPlayers(bTeam)
+}
