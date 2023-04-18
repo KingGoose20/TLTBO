12d99b668bc7b5ceae5075d1476c7cce3f1b18de
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-04-18 14:17:13 +1000

    1804

--- DocumentsAndIco/Array.js
+++ DocumentsAndIco/Array.js
@@ -1,22 +1,22 @@
 mainArray = {
   "Name":["Jasper Collier","Conor Farrington","Alexander Galt","Rudy Hoschke","Michael Iffland","Lukas Johnston","Clarrie Jones","William Kim","Samuel McConaghy","Ryan Pattemore","Nicholas Szogi","Christopher Tomkinson","Angus Walker","Willie Weekes","Mitch Yue","Sam James"],
   "Team":["5 Musketeers","Wet Willies","5 Musketeers","Wet Willies","Wet Willies","Wet Willies","Loose Gooses","Loose Gooses","5 Musketeers","5 Musketeers","5 Musketeers","Loose Gooses","Loose Gooses","Wet Willies","Loose Gooses","Loose Gooses"],
-  "PPG":[0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
-  "TP":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  "FPG":[0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
-  "TF":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  "MPG":[0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
-  "TM":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  "PPG":[0.00,1.00,2.00,1.00,1.00,0.00,0.00,1.00,3.00,0.00,1.00,0.00,1.00,1.00,0.00,0.00],
+  "TP":[0,1,2,1,1,0,0,1,3,0,1,0,1,1,0,0],
+  "FPG":[0.00,1.00,1.00,1.00,1.00,0.00,0.00,0.00,1.00,0.00,1.00,0.00,1.00,1.00,0.00,0.00],
+  "TF":[0,1,1,1,1,0,0,0,1,0,1,0,1,1,0,0],
+  "MPG":[0.00,0.00,1.00,0.00,0.00,0.00,0.00,1.00,2.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
+  "TM":[0,0,1,0,0,0,0,1,2,0,0,0,0,0,0,0],
   "TPG":[0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00,0.00],
   "TT":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
   "AccoladesOne":["","","Ex-GM","Ex-GM","Playmaker T1","MVP Runner Up T1","Champion T1","MVP T1","GM","Perimeter T1","The Biggest Bird","MIP T1","TLTBO CEO","Teammate T1","",""],
   "AccoladesTwo":["","","All-Defence Team T1","All-Offence Team T1","Thirdman T1","All-Offence Team T1","Finals MVP","All-Offence Team T1","All-Offence Team T1","Champion T1","Champion T1","","GM","Champion T1","",""],
   "AccoladesThree":["","","Champion T1","All-Defence Team T1","GM","","","All-Defence Team T1","All-Defence Team T","","","","All-Offence Team T1","","",""],
   "AccoladesFour":["","","","Champion T1","","","","","","","","","All-Defence Team T1","","",""],
-  "AccoladesFive":[0,0,0,0,0,0,0,0,0,0,0,0,"Scoring Champ T1",0,0,0],
-  "AccoladesSix":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  "AccoladesSeven":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  "AccoladesEight":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+  "AccoladesFive":["","","","","","","","","","","","","Scoring Champ T1","","",""],
+  "AccoladesSix":["","","","","","","","","","","","","","","",""],
+  "AccoladesSeven":["","","","","","","","","","","","","","","",""],
+  "AccoladesEight":["","","","","","","","","","","","","","","",""],
   "History":["Drafted by 5 Musketeers","Drafted by Wet Willies","Retained by 5 Musketeers","Retained by Wet Willies","","Drafted by Wet Willies","Drafted by Loose Gooses","Retained by Loose Gooses","","Drafted by 5 Musketeers","Drafted by 5 Musketeers","Drafted by Loose Gooses","","Drafted by Wet Willies","Drafted by Loose Gooses","Drafted by Loose Gooses"],
   "TeamImage":["../Images/5M_Final.png","../Images/WW_Final.png","../Images/5M_Final.png","../Images/WW_Final.png","../Images/WW_Final.png","../Images/WW_Final.png","../Images/LG_Final.png","../Images/LG_Final.png","../Images/5M_Final.png","../Images/5M_Final.png","../Images/5M_Final.png","../Images/LG_Final.png","../Images/LG_Final.png","../Images/WW_Final.png","../Images/LG_Final.png","../Images/LG_Final.png"],
   "PlayerImage":["../Images/Players/Jasper.jpeg","../Images/Players/Conor.jpeg","../Images/Players/Alex.jpeg","../Images/Players/Rudy.jpeg","../Images/Players/Michael.jpeg","../Images/Players/Lukas.jpeg","../Images/Players/Clarrie.jpeg","../Images/Players/Kimmy.jpeg","../Images/Players/SamM.jpeg","../Images/Players/Ryan.jpeg","../Images/Players/Nick.jpeg","../Images/Players/Chris.jpeg","../Images/Players/Angus.png","../Images/Players/Willie.jpeg","../Images/Players/Mitch.jpeg","../Images/Players/SamJ.jpg"]
@@ -24,8 +24,8 @@ mainArray = {
 
 preseasonOne = {
   "Date":["18-Apr"],
-  "Points":[5,4,5,4,5,2,3,0,1,3,2,4,2,5,5,4],
-  "Finishes":[1,1,5,0,2,3,4,3,5,3,0,1,2,5,5,1],
-  "Midrange":[2,1,2,4,3,2,0,0,1,5,5,2,3,1,3,1],
-  "ThreePointers":[3,2,0,6,5,3,1,5,1,3,2,4,5,5,1,1]
+  "Points":[0,1,2,1,1,0,0,1,3,0,1,0,1,1,0,0],
+  "Finishes":[0,1,1,1,1,0,0,0,1,0,1,0,1,1,0,0],
+  "Midrange":[0,0,1,0,0,0,0,1,2,0,0,0,0,0,0,0],
+  "ThreePointers":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 }
