cf1a7686b65f905b100a13e15e04cbd1ef8acff4
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-04-19 09:18:29 +1000

    d

--- Excel to Array/script1.js
+++ Excel to Array/script1.js
@@ -1,8 +1,8 @@
-const readXlsxFile = require('read-excel-file/node')
+import { readXlsxFile } from "https://unpkg.com/read-excel-file@5.x/bundle/read-excel-file.min.js";
  
 // File path.
 readXlsxFile("ExcelMain.xlsx").then((rows) => {
-    console.log("Hello")
+    console.log("Hellssso")
 // `rows` is an array of rows
 // each row being an array of cells.
 })
