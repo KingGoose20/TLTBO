2ed18e93131cd276a493f2e94dd6bc5cfb31a43d
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-04-25 11:02:12 +1000

    Index changes

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 var myVid = document.getElementById("backgroundVideo");
-switch (Math.floor(Math.random() * 5)) {
+  switch (Math.floor(Math.random() * 5)) {
   case 0:
+    myVid.poster = "Images/Poster/Decider Game.png"
     myVid.src = "Videos/Decider Game.mp4"
     break;
   case 1:
+    myVid.poster = "Images/Poster/Part 1.png"
     myVid.src = "Videos/Part 1.mp4"
     break;
   case 2:
+    myVid.poster = "Images/Poster/Part 2.png"
     myVid.src = "Videos/Part 2.mp4"
     break;
   case 3:
+    myVid.poster = "Images/Poster/Part 3.png"
     myVid.src = "Videos/Part 3.mp4"
     break;
   case 4:
+    myVid.poster = "Images/Poster/Part 4.png"
     myVid.src = "Videos/Part 4.mp4"
 }
