1add8d6b89569da5bb8a59b57fb6a37d80455443
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-05-16 12:39:21 +1000

    Update News.js

--- js/News.js
+++ js/News.js
@@ -1,97 +1,97 @@
 NewsTemplate = {
-    "ArticleName":[""],
-    "ArticleDescription":[""],
-    "ArticleDate":[""],
-    "ImageA":[""],
-    "ImageB":[""],
-    "ImageC":[""],
-    "ImageD":[""],
-    "ArticleLink":[""],
-    "ArticleType":[""]
+    "ArticleName": [""],
+    "ArticleDescription": [""],
+    "ArticleDate": [""],
+    "ImageA": [""],
+    "ImageB": [""],
+    "ImageC": [""],
+    "ImageD": [""],
+    "ArticleLink": [""],
+    "ArticleType": [""]
 }
 
 Preseason = {
-    "ArticleName":["Explaining the Term 2 Draft and Tournament", "Preseason Summary by Rudy Hoschke!"],
-    "ArticleDescription":["TLTBO CEO and GM Angus Walker breaks down how the second season of LTB will work, covering the draft, changes to the website, and changes to the game.", "EX-GM Rudy Hoschke has provided us with a detailed analysis on the results of the preseason, and what to expect from each team during the Term 2 Tournament."],
-    "ArticleDate":["15/04", "20/04"],
-    "ImageA":["Logo.png", "Logo.png"],
-    "ImageB":["", "WW_Final.png"],
-    "ImageC":["", ""],
-    "ImageD":["", ""],
-    "ArticleLink":["01.html", "02.html"],
-    "ArticleType":["Normal", "Normal"]
+    "ArticleName": ["Explaining the Term 2 Draft and Tournament", "Preseason Summary by Rudy Hoschke!"],
+    "ArticleDescription": ["TLTBO CEO and GM Angus Walker breaks down how the second season of LTB will work, covering the draft, changes to the website, and changes to the game.", "EX-GM Rudy Hoschke has provided us with a detailed analysis on the results of the preseason, and what to expect from each team during the Term 2 Tournament."],
+    "ArticleDate": ["15/04", "20/04"],
+    "ImageA": ["Logo.png", "Logo.png"],
+    "ImageB": ["", "WW_Final.png"],
+    "ImageC": ["", ""],
+    "ImageD": ["", ""],
+    "ArticleLink": ["01.html", "02.html"],
+    "ArticleType": ["Normal", "Normal"]
 }
 
 Week1 = {
-    "ArticleName":["All Lunch Team | Week 1 Day 1", "All Lunch Team | Week 1 Day 2"],
-    "ArticleDescription":["Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the tournament!", "Clarrie Jones has announced the All-Lunch top 5 offensive and defensive players from the second day of the tournament."],
-    "ArticleDate":["24/04", "26/04"],
-    "ImageA":["Logo.png", "Logo.png"],
-    "ImageB":["LG_Final.png", "LG_Final.png"],
-    "ImageC":["WW_Final.png", "WW_Final.png"],
-    "ImageD":["5M_Final.png", "5M_Final.png"],
-    "ArticleLink":["OD.html?Date=2404", "OD.html?Date=2604"],
-    "ArticleType":["Black", "Black"]
+    "ArticleName": ["All Lunch Team | Week 1 Day 1", "All Lunch Team | Week 1 Day 2"],
+    "ArticleDescription": ["Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the tournament!", "Clarrie Jones has announced the All-Lunch top 5 offensive and defensive players from the second day of the tournament."],
+    "ArticleDate": ["24/04", "26/04"],
+    "ImageA": ["Logo.png", "Logo.png"],
+    "ImageB": ["LG_Final.png", "LG_Final.png"],
+    "ImageC": ["WW_Final.png", "WW_Final.png"],
+    "ImageD": ["5M_Final.png", "5M_Final.png"],
+    "ArticleLink": ["OD.html?Date=2404", "OD.html?Date=2604"],
+    "ArticleType": ["Black", "Black"]
 }
 
 Week2 = {
-    "ArticleName":["All Lunch Team | Week 2 Day 1","All Lunch Team | Week 2 Day 2"],
-    "ArticleDescription":["Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the second week!","Clarrie Jones has announced the All-Lunch top 5 offensive and defensive players from the second day of the second week!"],
-    "ArticleDate":["2/05","3/05"],
-    "ImageA":["Logo.png", "Logo.png"],
-    "ImageB":["LG_Final.png", "LG_Final.png"],
-    "ImageC":["WW_Final.png", "WW_Final.png"],
-    "ImageD":["5M_Final.png", "5M_Final.png"],
-    "ArticleLink":["OD.html?Date=0205","OD.html?Date=0305"],
-    "ArticleType":["Black","Black"]
+    "ArticleName": ["All Lunch Team | Week 2 Day 1", "All Lunch Team | Week 2 Day 2"],
+    "ArticleDescription": ["Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the second week!", "Clarrie Jones has announced the All-Lunch top 5 offensive and defensive players from the second day of the second week!"],
+    "ArticleDate": ["2/05", "3/05"],
+    "ImageA": ["Logo.png", "Logo.png"],
+    "ImageB": ["LG_Final.png", "LG_Final.png"],
+    "ImageC": ["WW_Final.png", "WW_Final.png"],
+    "ImageD": ["5M_Final.png", "5M_Final.png"],
+    "ArticleLink": ["OD.html?Date=0205", "OD.html?Date=0305"],
+    "ArticleType": ["Black", "Black"]
 }
 
 Week3 = {
-    "ArticleName":["State of the Website","What is wrong with the Wet Willies?","The WW and LG Beef | AI Analyst","All Lunch Team | Week 3 Day 1","All Lunch Team | Week 3 Day 2","All Lunch Team | Week 3 Day 3"],
-    "ArticleDescription":["TLTBO CEO Angus Walker breaks down the changes that have occurred to the website over the term.<br><br><br>","Basketball mind Clarrie Jones breaks down the struggles of the Wet Willies, what they should've done, and how they can improve in the future.","Our official LTBO AI Analyst is back and better than ever! It has analysed the beef that is currently going on between the Wet Willies and the Loose Gooses, and provided a good explanation and viewpoint.","Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the third week!","Clarrie Jones of the Loose Gooses has announced the All-Lunch top 5 offensive and defensive players from the second day of the third week.","The top dog Clarrie Jones has announced the third All Teams from the third week!"],
-    "ArticleDate":["8/05","8/05","9/05","8/05","9/05","10/05"],
-    "ImageA":["Logo.png","LG_Final.png","Logo.png","Logo.png","Logo.png","Logo.png"],
-    "ImageB":["LG_Final.png","Logo.png","","LG_Final.png","LG_Final.png","LG_Final.png"],
-    "ImageC":["","WW_Final.png","","WW_Final.png","WW_Final.png","WW_Final.png"],
-    "ImageD":["","","","5M_Final.png","5M_Final.png","5M_Final.png"],
-    "ArticleLink":["03.html","04.html","05.html","OD.html?Date=0805","OD.html?Date=0905","OD.html?Date=1005"],
-    "ArticleType":["White","Normal","White","Black","Black","Black"]
+    "ArticleName": ["State of the Website", "What is wrong with the Wet Willies?", "The WW and LG Beef | AI Analyst", "All Lunch Team | Week 3 Day 1", "All Lunch Team | Week 3 Day 2", "All Lunch Team | Week 3 Day 3"],
+    "ArticleDescription": ["TLTBO CEO Angus Walker breaks down the changes that have occurred to the website over the term.<br><br><br>", "Basketball mind Clarrie Jones breaks down the struggles of the Wet Willies, what they should've done, and how they can improve in the future.", "Our official LTBO AI Analyst is back and better than ever! It has analysed the beef that is currently going on between the Wet Willies and the Loose Gooses, and provided a good explanation and viewpoint.", "Clarrie Jones has announced his All-Lunch top 5 offensive and defensive players from the first day of the third week!", "Clarrie Jones of the Loose Gooses has announced the All-Lunch top 5 offensive and defensive players from the second day of the third week.", "The top dog Clarrie Jones has announced the third All Teams from the third week!"],
+    "ArticleDate": ["8/05", "8/05", "9/05", "8/05", "9/05", "10/05"],
+    "ImageA": ["Logo.png", "LG_Final.png", "Logo.png", "Logo.png", "Logo.png", "Logo.png"],
+    "ImageB": ["LG_Final.png", "Logo.png", "", "LG_Final.png", "LG_Final.png", "LG_Final.png"],
+    "ImageC": ["", "WW_Final.png", "", "WW_Final.png", "WW_Final.png", "WW_Final.png"],
+    "ImageD": ["", "", "", "5M_Final.png", "5M_Final.png", "5M_Final.png"],
+    "ArticleLink": ["03.html", "04.html", "05.html", "OD.html?Date=0805", "OD.html?Date=0905", "OD.html?Date=1005"],
+    "ArticleType": ["White", "Normal", "White", "Black", "Black", "Black"]
 }
 
 Week4 = {
-    "ArticleName":["The TLTBO All Teams | Week 4 Day 1","What is wrong with the Wet Willies? Part 2!"],
-    "ArticleDescription":["Loose Gooses guard Clarrie Jones has released the All-Teams from the first day of Week 4, headlined by the Wet Willies Big 3.","Dumbass Samuel McConaghy proves his ability to 'poke the bear', talking shit to the Wet Willies once again."],
-    "ArticleDate":["15/05","15/05"],
-    "ImageA":["Logo.png","Logo.png"],
-    "ImageB":["LG_Final.png","WW_Final.png"],
-    "ImageC":["WW_Final.png","5M_Final.png"],
-    "ImageD":["",""],
-    "ArticleLink":["OD.html?Date=1505","06.html"],
-    "ArticleType":["Black","Normal"]
+    "ArticleName": ["The TLTBO All Teams | Week 4 Day 1", "What is wrong with the Wet Willies? Part 2!", "Crunching Numbers | Term 2"],
+    "ArticleDescription": ["Loose Gooses guard Clarrie Jones has released the All-Teams from the first day of Week 4, headlined by the Wet Willies Big 3.", "Dumbass Samuel McConaghy proves his ability to 'poke the bear', talking shit to the Wet Willies once again.", "TLTBO CEO Angus Walker is back with the first 'Crunching Numbers' from the second season of lunchtime basketball."],
+    "ArticleDate": ["15/05", "15/05", "16/05"],
+    "ImageA": ["Logo.png", "Logo.png", "Logo.png"],
+    "ImageB": ["LG_Final.png", "WW_Final.png", ""],
+    "ImageC": ["WW_Final.png", "5M_Final.png", ""],
+    "ImageD": ["", "", ""],
+    "ArticleLink": ["OD.html?Date=1505", "06.html", "07.html"],
+    "ArticleType": ["Black", "Normal", "White"]
 }
 
 Week5 = {
-    "ArticleName":[],
-    "ArticleDescription":[],
-    "ArticleDate":[],
-    "ImageA":[],
-    "ImageB":[],
-    "ImageC":[],
-    "ImageD":[],
-    "ArticleLink":[],
-    "ArticleType":[]
+    "ArticleName": [],
+    "ArticleDescription": [],
+    "ArticleDate": [],
+    "ImageA": [],
+    "ImageB": [],
+    "ImageC": [],
+    "ImageD": [],
+    "ArticleLink": [],
+    "ArticleType": []
 }
 
 Finals = {
-    "ArticleName":[],
-    "ArticleDescription":[],
-    "ArticleDate":[],
-    "ImageA":[],
-    "ImageB":[],
-    "ImageC":[],
-    "ImageD":[],
-    "ArticleLink":[],
-    "ArticleType":[]
+    "ArticleName": [],
+    "ArticleDescription": [],
+    "ArticleDate": [],
+    "ImageA": [],
+    "ImageB": [],
+    "ImageC": [],
+    "ImageD": [],
+    "ArticleLink": [],
+    "ArticleType": []
 }
 
 numberOfElements = 0
@@ -105,7 +105,7 @@ function createArticles(variableToUse, elementID) {
     template = document.getElementsByTagName("template")[0];
     rowToUse = ""
     for (i = 0; i < variableToUse.ArticleName.length; i++) {
-        if ((i%2) == 0) {
+        if ((i % 2) == 0) {
             rowToUse = document.createElement("div")
             rowToUse.id = i
             rowToUse.classList.add("NewsHeader")
@@ -116,15 +116,15 @@ function createArticles(variableToUse, elementID) {
         clone.getElementById("Date").innerHTML = variableToUse.ArticleDate[i]
         clone.getElementById("Link").href = "NewsPages/" + variableToUse.ArticleLink[i]
         switch (variableToUse.ArticleType[i]) {
-            case "Normal" :
+            case "Normal":
                 break;
-            case "White" :
+            case "White":
                 clone.getElementById("startImages").style.backgroundColor = "rgb(230,230,230)";
                 clone.getElementById("Title").style.color = "rgb(0,0,0)"
                 clone.getElementById("Description").style.color = "rgb(0,0,0)"
                 clone.getElementById("Date").style.color = "rgb(0,0,0)"
                 break;
-            case "Black" :
+            case "Black":
                 clone.getElementById("startImages").style.backgroundColor = "rgb(0,0,0)";
                 break;
         }
@@ -153,23 +153,23 @@ function createArticles(variableToUse, elementID) {
             clone.getElementById("ImageD").remove();
         }
         if ((variableToUse.ArticleName.length % 2) == 0) {
-            if ((i % 2) != 0 ) {
-                
+            if ((i % 2) != 0) {
+
                 clone.getElementById("Header").style.float = "left";
             } else {
-                
+
                 clone.getElementById("Header").style.float = "right";
             }
         } else {
-            if (i == (variableToUse.ArticleName.length-1)) {
-            clone.getElementById("Header").style.width = "100%";
-            clone.getElementById("Header").style.float = "position";
-            clone.getElementById("Header").style.marginBottom = "50px"
-        } else if ((i % 2) == 0 ) {
-            clone.getElementById("Header").style.float = "right";
-        } else {
-            clone.getElementById("Header").style.float = "left";
-        }
+            if (i == (variableToUse.ArticleName.length - 1)) {
+                clone.getElementById("Header").style.width = "100%";
+                clone.getElementById("Header").style.float = "position";
+                clone.getElementById("Header").style.marginBottom = "50px"
+            } else if ((i % 2) == 0) {
+                clone.getElementById("Header").style.float = "right";
+            } else {
+                clone.getElementById("Header").style.float = "left";
+            }
         }
 
         clone.getElementById("Header").id = "Element" + numberOfElements
